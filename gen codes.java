c08fab0ac49751f03205b076f6ebd8e84287e033
APD <63363238+apd2019@users.noreply.github.com>
2024-03-30 12:28:06 +0530

    Update gen codes.java

--- gen codes.java
+++ gen codes.java
@@ -1,4 +1,23 @@
+//Driver invokation
+
 WebDriver driver = new ChromeDriver();
+
+//pkg (related)
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 WebDriver driver = new FirefoxDriver();
 WebDriver driver = new EdgeDriver();
 WebDriver driver = new SafariDriver();
+
+===========================================
+
+//Use WebElement if you need to point to a component or element of the page.
+//Like if you are targeting the header or footer in most of the cases
+//To narrow down the scope to that element or section only
+//for searching benefits
+
+WebElement foot = driver.findElement(By.id("gf-BIG")); //[foot = variable or object that holds the section reference]
+
+===============================================
