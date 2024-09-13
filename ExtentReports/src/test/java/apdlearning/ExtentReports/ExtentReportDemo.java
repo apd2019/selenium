53c1d80c204e786ebb166efd2ecb6f0a989db58c
arkap <arkap@LAPTOP-86AV4P6R>
2024-09-14 00:12:09 +0530

    extent report

--- ExtentReports/src/test/java/apdlearning/ExtentReports/ExtentReportDemo.java
+++ ExtentReports/src/test/java/apdlearning/ExtentReports/ExtentReportDemo.java
@@ -1,7 +1,12 @@
 package apdlearning.ExtentReports;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -15,7 +20,7 @@ public class ExtentReportDemo {
 	ExtentReports extent;
 	
 	@BeforeTest
-	public void config() {
+	public void config() throws IOException {
 		//ExtentReports, ExtentSparkReporter
 		String path = System.getProperty("user.dir")+"\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
@@ -25,15 +30,23 @@ public class ExtentReportDemo {
 		ExtentReports extent = new ExtentReports();
 		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "APD");
+		extent.flush();
+		Desktop.getDesktop().browse(new File(path).toURI());
 		
 	}
 	@Test
 	public void demoInit() {
 		extent.createTest("demo initial");
-		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64");
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/");
 		System.out.println(driver.getTitle());
-		extent.flush();
+		
+		
 	}
+	
+//	@AfterTest
+//	public void flush() throws IOException {
+//		Desktop.getDesktop().browse(new File("index.html").toURI());
+//	}
 }
